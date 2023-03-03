a8e71a155c1d75238560d7b2b0c5c8e4e130be2e
DREWiex <aleon88@gmail.com>
2023-03-03 07:34:43 +0100

    Ejercicio 5 casi terminado

--- 02/ejercicios con objetos/ejercicio_05/main.js
+++ 02/ejercicios con objetos/ejercicio_05/main.js
@@ -2,11 +2,36 @@ class Ficha {
     constructor(nombre){
         this.nombre = nombre,
         this.km = [],
-        this.sesiones = 0
+        this.sesiones;
+        this.anotar = function(...km){
+            
+            this.km.push(...km);
+
+            this.sesiones = this.km.length;
+
+            console.log(this.km);
+            console.log(this.sesiones);
+        };
+        this.media = function(km, sesiones){
+            
+            let total = 0;
+        
+            let calcularMedia = this.km.forEach((item) => (total += item) / sesiones);
+    
+            return calcularMedia;    
+        };
     };
 
-    anotar(){
+    anotar(...km){
 
+        this.km.push(...km);
+
+        this.sesiones = this.km.length;
+
+        console.log(this.km);
+        console.log(this.sesiones);
+
+        /*
         do{
             this.sesiones = parseInt(prompt("¿Cuántas sesiones de ejercicios realizaste?"));
             if(isNaN(this.sesiones)){
@@ -18,7 +43,7 @@ class Ficha {
             let pregunta = prompt(`¿Cuántos kilómetros recorriste en la sesión ${i}?`);
             this.km.push(pregunta);
         }
-
+        */
     };
 
     media(){
@@ -34,4 +59,5 @@ class Ficha {
 };
 
 const fichaJorge = new Ficha('Jorge');
-fichaJorge.anotar();
+fichaJorge.anotar(8,5,10);
+console.log(fichaJorge.media(this.km, this.sesiones));
